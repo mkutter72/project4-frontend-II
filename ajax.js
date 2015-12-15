c607d18dad541912ef48ba2f71090ba4aef476a1
MikeKutter <mkutter@yahoo.com>
2015-12-15 15:52:56 -0500

    Integrating old and new front end

---
+++ ajax.js
@@ -0,0 +1,150 @@
+'use strict';
+var externAppsFunctions = externAppsFunctions || {};
+
+function fillCalendarCallback(error, data) {
+    if (error) {
+
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+
+    // keeping this around just incase I need to turn back on displaying all returns from DB
+    var dataStr = JSON.stringify(data, null, 4);
+
+    var oneThing = data[0];
+    var size = data.length;
+    for (var i = 0; i < data.length; i++)
+        externAppsFunctions.addCalendarEvent(data[i]["date"],data[i]["userName"],data[i]["description"],data[i]["time"]);
+
+  };
+
+function generalCallback(error, data) {
+    if (error) {
+
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+
+    // keeping this around just incase I need to turn back on displaying all returns from DB
+    var dataStr = JSON.stringify(data, null, 4);
+
+  };
+
+var api = {
+  url: 'http://localhost:3000',
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+  register: function(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/signup',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials)
+      // dataType: 'json'
+    }, callback);
+  },
+  login: function(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/login',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials)
+    }, callback);
+  },
+
+  getCalendar: function (callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/appointment',
+    }, callback);
+  },
+
+  createAppointment: function(appointmentData, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/appointment/makenew',
+      contentType: 'application/json',
+      data: JSON.stringify(appointmentData)
+    }, callback);
+  },
+
+ createMessageBoard: function(boardData, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/mboard/makenew',
+      contentType: 'application/json',
+      data: JSON.stringify(boardData)
+    }, callback);
+  },
+
+  updateMessageBoard: function(messageData, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.url + '/mboard/update',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(messageData)
+    }, callback);
+  },
+
+
+
+
+  createSurvey: function(surveyData, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/survey/makenew',
+      contentType: 'application/json',
+      data: JSON.stringify(surveyData)
+    }, callback);
+  },
+  loadSurvey: function(surveyURL, callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/survey' + surveyURL,
+      contentType: 'application/json',
+      dataType: 'json'
+    }, callback);
+  },
+  createResult: function(surveyData, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/result/makenew',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(surveyData)
+    }, callback);
+  },
+  updateResult: function(surveyData, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.url + '/result/update',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(surveyData)
+    }, callback);
+  },
+  getResults: function (surveyName, callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/result?q=' + surveyName,
+      dataType: 'json'
+    }, callback);
+  },
+
+  deleteResults: function (surveyName, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/result/destroy?q=' + surveyName,
+    }, callback);
+  },
+
+  deleteSurvey: function (surveyName, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/survey/destroy?q=' + surveyName,
+    }, callback);
+  },
+};
---
+++ calendar.js
@@ -0,0 +1,170 @@
+'use strict';
+
+var externAppsFunctions = externAppsFunctions || {};
+
+var allEvents;
+
+function addLeadingZero (num) {
+ if (num < 10) {
+   return "0" + num;
+ } else {
+   return "" + num;
+ }
+};
+
+function addCalendarEvent(key,userName,description,time)
+{
+  var events = allEvents.options.events;
+  var currentAppointments = {}
+  currentAppointments[key] = events[key];
+
+  if (events[key]) {
+    currentAppointments[key].number +=  1;
+    var newindex = currentAppointments[key].dayEvents.length;
+    var newAppointment = {
+      "userName": userName,
+      "description": description,
+      "time": time
+    };
+
+    currentAppointments[key].dayEvents[newindex] = newAppointment;
+
+    $(".responsive-calendar").responsiveCalendar('edit', currentAppointments);
+  } else {
+
+    var editData = {};
+    editData[key] = {
+      "number": 1,
+      "dayEvents": [{
+        "userName": userName,
+        "description": description,
+        "time": time
+      }]
+    };
+
+    $(".responsive-calendar").responsiveCalendar('edit', editData);
+  };
+};
+
+
+
+var form2object = function(form) {
+  var data = {};
+  $(form).find("input").each(function(index, element) {
+    var type = $(this).attr('type');
+    if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+      data[$(this).attr('name')] = $(this).val();
+    }
+  });
+  return data;
+};
+
+$(document).ready(function () {
+  $('#showEvents').on('click',function (event){
+    var resultString = JSON.stringify(allEvents.options.events);
+    $('#result').val(resultString);
+  });
+
+  $('#createBoardID').on('click',function (e){
+    var boardData = {
+      "boardname": $('#boardNameID').val()
+        };
+
+    e.preventDefault();
+    api.createMessageBoard(boardData,generalCallback);
+    });
+
+ $('#postMessageID').on('click',function (e){
+   var messageData = {
+      "boardname": $('#boardNameID').val(),
+      "messagetext": $('#messageTextID').val()
+        };
+
+    e.preventDefault();
+    api.updateMessageBoard(messageData,generalCallback);
+  });
+
+
+  $('#create-appointment').on('submit', function(e) {
+    e.preventDefault();
+    var appointmentData = form2object(this);
+
+    var events = allEvents.options.events;
+    var currentAppointments = {}
+    var key = appointmentData.appDate;
+    currentAppointments[key] = events[key];
+
+    if (events[key]) {
+      currentAppointments[key].number +=  1;
+      var newindex = currentAppointments[key].dayEvents.length;
+      var newAppointment = {
+        "userName": appointmentData.appUsername,
+        "description": appointmentData.appDescription,
+        "time": appointmentData.appTime
+      };
+
+      currentAppointments[key].dayEvents[newindex] = newAppointment;
+
+      $(".responsive-calendar").responsiveCalendar('edit', currentAppointments);
+    } else {
+
+      var editData = {};
+      editData[appointmentData.appDate] = {
+        "number": 1,
+        "dayEvents": [{
+          "userName": appointmentData.appUsername,
+          "description": appointmentData.appDescription,
+          "time": appointmentData.appTime
+        }]
+      };
+
+      $(".responsive-calendar").responsiveCalendar('edit', editData);
+    }
+    $('#create-appointment').trigger("reset");
+    api.createAppointment(appointmentData,generalCallback);
+  });
+
+$('#clearEvents').on('click',function (){
+  console.log("got click");
+
+  $(".responsive-calendar").responsiveCalendar('clearAll');
+
+});
+
+
+$(".responsive-calendar").responsiveCalendar({
+  time: '2015-12',
+  startFromSunday: true,
+  allRows: false,
+
+  onDayHover: function(events) {
+    var key = $(this).data('year')+'-'+addLeadingZero( $(this).data('month') )+'-'+addLeadingZero( $(this).data('day') );
+    var  thisDayEvent = events[key];
+
+    var resultString = key;
+
+    if (thisDayEvent) {
+      resultString += JSON.stringify(thisDayEvent);
+      }
+    $('#result').val(resultString);
+    },
+
+
+  onDayClick: function(events) {
+    var key = $(this).data('year')+'-'+addLeadingZero( $(this).data('month') )+'-'+addLeadingZero( $(this).data('day') );
+
+    $("#appointment-date").val(key);
+
+    },
+
+  onInit: function() {
+   allEvents = this;
+   api.getCalendar(fillCalendarCallback);
+    }
+
+  })
+});
+
+
+externAppsFunctions['addCalendarEvent'] = addCalendarEvent;
+
