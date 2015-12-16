aa48699c19931c34f5a39a23fa35736c4f3a491d
MikeKutter <mkutter@yahoo.com>
2015-12-16 14:03:16 -0500

    Fixed heroku path

--- ajax.js
+++ ajax.js
@@ -59,7 +59,7 @@ function generalCallback(error, data) {
   };
 
 var api = {
-  url: 'https://intense-cove-9531.herokuapp.com/',
+  url: 'https://intense-cove-9531.herokuapp.com',
   ajax: function(config, cb) {
     $.ajax(config).done(function(data, textStatus, jqxhr) {
       cb(null, data);
