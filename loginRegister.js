ab6f35aaa6257e36786c0cce7f2bd26bc40f2993
MikeKutter <mkutter@yahoo.com>
2015-12-16 10:01:09 -0500

    Refactoring

--- loginRegister.js
+++ loginRegister.js
@@ -58,3 +58,17 @@ $('#logOut').on('click', function(e) {
   });
 
 
+ $('#logMeIn').on('submit', function(e) {
+    e.preventDefault();
+
+    var credentials = form2object(this);
+
+    var loginCallback = function(error, data){
+      if(error){
+        console.log(error);
+      } else {
+        window.location.href = 'indexFull.html'
+      }
+    };
+    api.login(credentials, loginCallback);
+});
--- messages.js
+++ messages.js
@@ -8,47 +8,38 @@ var socket = io('http://localhost:3030');
 
 
 
-var form2object = function(form) {
-  var data = {};
-  $(form).find("input").each(function(index, element) {
-    var type = $(this).attr('type');
-    if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
-      data[$(this).attr('name')] = $(this).val();
-    }
-  });
-  return data;
-};
-
 $(document).ready(function () {
   $('#showEvents').on('click',function (event){
+    e.preventDefault();
     var resultString = JSON.stringify(allEvents.options.events);
     $('#result').val(resultString);
   });
 
   $('#createBoardID').on('click',function (e){
+    e.preventDefault();
     var boardData = {
       "boardname": $('#boardNameID').val()
         };
 
-    e.preventDefault();
     api.createMessageBoard(boardData,generalCallback);
     });
 
  $('#postMessageID').on('click',function (e){
-   var messageData = {
+    e.preventDefault();
+    var messageData = {
       "boardname": $('#boardNameID').val(),
       "messagetext": $('#messageTextID').val()
-        };
+      };
 
-    e.preventDefault();
-    api.updateMessageBoard(messageData,generalCallback);
+
     socket.emit('chat message', $('#messageTextID').val());
-  });
+    api.updateMessageBoard(messageData,generalCallback);
+    });
 
   $('#seleectBoardID').on('click',function (e){
     e.preventDefault();
     api.getMessageBoard($('#boardNameID').val(),displayMessagesCallback);
-  });
+    });
 
   socket.on('chat message', function(msg){
       $('#chatspace').val(msg);
