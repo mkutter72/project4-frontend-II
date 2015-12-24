0fd975bf1bbd9e783b16279642c396f1267cc89e
MikeKutter <mkutter@yahoo.com>
2015-12-24 16:51:51 -0500

    Added sending command to clear date in database.  Cleaned out old code and added "ADD" string to add date message for socket.io

--- ajax.js
+++ ajax.js
@@ -187,60 +187,13 @@ var api = {
     }, callback);
   },
 
-
-
-
-
-
-
-
-  createSurvey: function(surveyData, callback) {
-    this.ajax({
-      method: 'POST',
-      url: this.url + '/survey/makenew',
-      contentType: 'application/json',
-      data: JSON.stringify(surveyData)
-    }, callback);
-  },
-  loadSurvey: function(surveyURL, callback) {
-    this.ajax({
-      method: 'GET',
-      url: this.url + '/survey' + surveyURL,
-      contentType: 'application/json',
-      dataType: 'json'
-    }, callback);
-  },
-  createResult: function(surveyData, callback) {
-    this.ajax({
-      method: 'POST',
-      url: this.url + '/result/makenew',
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(surveyData)
-    }, callback);
-  },
-  updateResult: function(surveyData, callback) {
-    this.ajax({
-      method: 'PATCH',
-      url: this.url + '/result/update',
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(surveyData)
-    }, callback);
-  },
-  getResults: function (surveyName, callback) {
-    this.ajax({
-      method: 'GET',
-      url: this.url + '/result?q=' + surveyName,
-      dataType: 'json'
-    }, callback);
-  },
-
-
-  deleteSurvey: function (surveyName, callback) {
+  clearDate: function (dateStr, callback) {
     this.ajax({
       method: 'DELETE',
-      url: this.url + '/survey/destroy?q=' + surveyName,
+      url: this.url + '/appointment/destroy?q=' + dateStr,
     }, callback);
   },
+
 };
 
 
